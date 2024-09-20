0498beb34d14ecf6374b53f1be0a81d03599abca
ArkanioGX <86797763+ArkanioGX@users.noreply.github.com>
2024-09-20 13:00:48 +0200

    Final Correction

---
+++ Source/Projec_M4_SkillTest/Public/ST_GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "ST_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJEC_M4_SKILLTEST_API AST_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
